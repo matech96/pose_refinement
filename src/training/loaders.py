f38da831a03033b3e446e34f8cc8c702b4674e1c
matech96 <gangoly96@gmail.com>
2020-07-28 12:52:36 +0000

    bugfix

--- src/training/loaders.py
+++ src/training/loaders.py
@@ -126,8 +126,8 @@ class ChunkedGenerator:
                 # batch_inds = indices[ind]  # (nBatch,)
                 batch_frame_start = self.frame_start[batch_inds][:, np.newaxis]
                 batch_frame_end = self.frame_end[batch_inds][:, np.newaxis]
-                assert len(np.unique(batch_frame_start)) == 1
-                assert len(np.unique(batch_frame_end)) == 1
+                # assert len(np.unique(batch_frame_start)) == 1
+                # assert len(np.unique(batch_frame_end)) == 1
 
                 if self.augment:
                     flip = np.random.random(len(batch_inds)) < 0.5
