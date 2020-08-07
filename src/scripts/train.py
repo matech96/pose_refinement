159351aceac91d6c3fdcc049108db9949fa8d335
matech96 <gangoly96@gmail.com>
2020-08-07 09:23:16 +0000

    layernorm = "none"

--- src/scripts/train.py
+++ src/scripts/train.py
@@ -254,7 +254,7 @@ if __name__ == "__main__":
                 output_path = args.output
 
             params = {
-                "num_epochs": 1,#5,
+                "num_epochs": 15,
                 "preprocess_2d": "DepthposeNormalize2D",
                 "preprocess_3d": "SplitToRelativeAbsAndMeanNormalize3D",
                 "shuffle": True,
